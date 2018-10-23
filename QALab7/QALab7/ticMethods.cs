910a3144868750f54293c619360c923cbc717b9c
Jacob P Davis <tug09022@temple.edu>
2018-10-23 18:59:12 -0400

    first commit

---
+++ QALab7/QALab7/ticMethods.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace QALab7
+{
+    public class ticMethods
+    {
+        string[,] tictac;
+
+        //Creating Board
+        public Array createBoard()
+        {
+            tictac = new string[3,3];
+            return tictac;
+        }
+
+
+        //Getting Current Player
+        public string currentPlayer(int player)
+        {
+            if(player == 1)
+            {
+                return "player1";
+            }
+            if(player == 2)
+            {
+                return "player2";
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+
+        //Changing PlayAAA
+        public string changePlayer(int currentPlayer)
+        {
+            if(currentPlayer == 1)
+            {
+                return "player1";
+            }
+            if (currentPlayer == 2)
+            {
+                return "player2";
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+
+        //Making mewvs outchea
+        public string makeMove(int playa, int row, int col)
+        {
+            createBoard();
+            if(playa == 1)
+            {
+                tictac[row, col] = "x";
+                return "x";
+            }
+            if(playa == 2)
+            {
+                tictac[row, col] = "o";
+                return "o";
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public bool spaceInUse(int row, int col)
+        {
+            createBoard();
+            if (tictac[row, col] == "o")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ QALab7/QALab7Tests/ticMethodsTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QALab7;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QALab7.Tests
+{
+    [TestClass()]
+    public class ticMethodsTests
+    {
+        ticMethods test = new ticMethods();
+
+        [TestMethod()]
+        public void createBoardTest()
+        {
+            int expected = 9;
+            int actual = test.createBoard().Length;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void currentPlayerTest()
+        {
+            string expected = "player1";
+            string actual = test.currentPlayer(1);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void currentPlayerTest2()
+        {
+            string expected = "player2";
+            string actual = test.currentPlayer(2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void changePlayerTest()
+        {
+            string expected = "player2";
+            string actual = test.changePlayer(2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void makeMoveTest()
+        {
+            string expected = "o";
+            string actual = test.makeMove(2, 1, 1);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void spaceInUseTest()
+        {
+            bool expected = false;
+            bool actual = test.spaceInUse(1,1);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
